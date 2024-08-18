332c31adc3da0addcfc263ca3ffb5f20e5e847e0
gckneip <gckneip@inf.ufpel.edu.br>
2024-08-18 17:03:27 -0300

    tentando criar o menu

--- src/dungeonfighter/Batalha.java
+++ src/dungeonfighter/Batalha.java
@@ -1,17 +1,14 @@
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class Batalha extends JFrame {
+public class Batalha extends JPanel {
 
     private final JButton botaoAtacar;
     private final JButton botaoFugir;
     private final JButton botaoUsarItem;
 
     public Batalha() {
-        super("Batalha");
-
         botaoAtacar = new JButton("Atacar");
         botaoFugir = new JButton("Fugir");
         botaoUsarItem = new JButton("Usar Item");
@@ -24,7 +21,6 @@ public class Batalha extends JFrame {
         add(painel);
 
         setSize(300, 200);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
     }
 }
--- src/dungeonfighter/DungeonFighter.java
+++ src/dungeonfighter/DungeonFighter.java
@@ -1,71 +1,38 @@
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import menu.Menu;
 
-public class DungeonFighter {
+public class DungeonFighter extends JFrame {
 
-    private JFrame menu;
-    private JFrame batalha;
-    private JFrame tabuleiro;
+    private final Menu menu;
+    private final Batalha batalha;
+    private final Tabuleiro tabuleiro;
 
     public DungeonFighter() {
-        // Initialize the frames
+        super("Dungeon Fighter");
+
         menu = new Menu();
-        batalha = new JFrame("Frame 2");
-        tabuleiro = new JFrame("Frame 3");
-
-        batalha.setLayout(new FlowLayout());
-        tabuleiro.setLayout(new FlowLayout());
-
-        // Create the buttons
-        JButton button1 = new JButton("Go to Frame 2");
-        JButton button2 = new JButton("Go to Frame 3");
-        JButton button3 = new JButton("Go to Frame 1");
-
-        // Add action listeners to the buttons
-        button1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                menu.setVisible(false);
-                batalha.setVisible(true);
-            }
-        });
-
-        button2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                batalha.setVisible(false);
-                tabuleiro.setVisible(true);
-            }
-        });
-
-        button3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                tabuleiro.setVisible(false);
-                menu.setVisible(true);
-            }
-        });
-
-        // Add the buttons to the frames
-        menu.add(button1);
-        batalha.add(button2);
-        tabuleiro.add(button3);
-
-        // Set default close operation
-        menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        batalha.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        tabuleiro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // Set frame sizes
-        menu.setSize(300, 200);
-        batalha.setSize(300, 200);
-        tabuleiro.setSize(300, 200);
-
-        // Make menu visible initially
+        batalha = new Batalha();
+        tabuleiro = new Tabuleiro();
+        
+        getContentPane().setLayout(new CardLayout());
+        getContentPane().add(menu, "Menu");
+        getContentPane().add(batalha, "Batalha");
+        getContentPane().add(tabuleiro, "Tabuleiro");
+        
         menu.setVisible(true);
+        batalha.setVisible(false);
+        tabuleiro.setVisible(false);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH); 
     }
 
     public static void main(String[] args) {
-        new DungeonFighter();
+        JFrame jogo = new DungeonFighter();
+        jogo.setVisible(true);
     }
 }
