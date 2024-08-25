a7c2b7d6ad9e6906487794a35bb91bf38b4bd5e4
gckneip <gckneip@inf.ufpel.edu.br>
2024-08-25 14:28:04 -0300

    consertando scoobyLoo

--- src/dungeonfighter/entidades/personagens/scoobyLoo.java
+++ src/dungeonfighter/entidades/personagens/scoobyLoo.java
@@ -1,9 +1,10 @@
 package dungeonfighter.entidades.personagens;
 
+import dungeonfighter.entidades.itens.Elixir;
 import javax.swing.ImageIcon;
 
 public class scoobyLoo extends Inimigo {
     public scoobyLoo() {
-        super("Scooby Loo", 20, 10, 0, new ImageIcon ("src/dungeonfighter/assets/scoobyLoo.png"), true);
+        super("Scooby Loo", 20, 10, 0, new ImageIcon ("src/dungeonfighter/assets/scoobyLoo.png"), true, new Elixir(true, 5));
     }
 }
